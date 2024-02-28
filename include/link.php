d405a384bb3eba5d26c64cf30e628e8533c6d92f
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-02-28 12:00:16 +0800

    Add include/link.php (committed on 2024-02-28 12:00:16)

---
+++ include/link.php
@@ -0,0 +1,16 @@
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<!-- FontAwesome Icons -->
+<link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+<!-- Third-party Styles -->
+<link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+<link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+<!-- Framework CSS (SB Admin 2) -->
+<link href="../assets/css/sb-admin-2.min.css" rel="stylesheet" type="text/css">
+
+<!-- Custom Styles -->
+<link href="../assets/css/style.css" rel="stylesheet" type="text/css">
