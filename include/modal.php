071b52a34f6f4aa940a1c4738b3efd81765af7c5
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-03-07 13:22:15 +0800

    Add include/modal.php (committed on 2024-03-07 13:22:15)

---
+++ include/modal.php
@@ -0,0 +1,14 @@
+<div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addSupplierModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header bg-primary text-white">
+        <h5 class="modal-title" id="addSupplierModalLabel">Add Supplier</h5>
+        <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form id="addForm">
+      </form>
+    </div>
+  </div>
+</div>
