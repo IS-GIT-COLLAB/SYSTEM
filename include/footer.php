af92cd20e0f6207aa2178165263abf8f3855755f
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-06-25 15:29:24 +0800

    Add include/footer.php (committed on 2024-06-25 15:29:24)

---
+++ include/footer.php
@@ -0,0 +1,33 @@
+</div>
+<!-- End of Main Content -->
+
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>&copy; GPI (Information System). All rights reserved.</span>
+    </div>
+  </div>
+</footer>
+<!-- End of Footer -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+  <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Modals -->
+<?php include 'modal.php'; ?>
+
+<!-- Bootstrap core JavaScript-->
+<?php include 'script.php'; ?>
+
+</body>
+
+</html>
