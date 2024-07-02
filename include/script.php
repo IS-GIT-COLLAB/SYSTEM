5f47bfec17f39962b05065cbd0647a1613541b31
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2024-07-02 10:30:10 +0800

    Add include/script.php (committed on 2024-07-02 10:30:10)

---
+++ include/script.php
@@ -0,0 +1,30 @@
+<!-- jQuery -->
+<script src="../vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Bundle (includes Popper.js) -->
+<script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- jQuery Easing (used by SB Admin 2) -->
+<script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- SB Admin 2 Script -->
+<script src="../assets/js/sb-admin-2.min.js"></script>
+
+<!-- DataTables Scripts -->
+<script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+<!-- Moment.js (for date handling) -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+
+<!-- DataTables DateTime Sorting Plugin (depends on Moment.js) -->
+<script src="https://cdn.datatables.net/plug-ins/1.13.4/sorting/datetime-moment.js"></script>
+
+<!-- Select2 Plugin -->
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+
+<!-- Bootstrap Select Plugin -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+
+<!-- Custom Scripts (Should be last) -->
+<script src="../assets/js/script.js"></script>
